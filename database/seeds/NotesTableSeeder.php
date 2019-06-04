a31984f1a939daad400efc9bad5cf63a6da6284c
Bálint Dózsa <balint.do89@gmail.com>
2019-06-04 21:39:12 +0200

    Add seeds

---
+++ database/seeds/NotesTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class NotesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('notes')->insert([
+            'user_id' => 1,
+            'note' => 'Test Note',
+        ]);
+    }
+}
