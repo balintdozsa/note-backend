1ab2874052c7f630c1f0876f753ac2eebd2ac87f
Bálint Dózsa <balint.do89@gmail.com>
2019-06-05 15:58:37 +0200

    Use Passport tokens

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -17,13 +18,24 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            $token = Str::random(60);
+            /*$token = Str::random(60);
 
             $request->user()->forceFill([
                 'api_token' => hash('sha256', $token),
-            ])->save();
+            ])->save();*/
 
-            return ['token' => $token,];
+            $user = $request->user();
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+            $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->save();
+
+            return [
+                'token' => $tokenResult->accessToken,
+                'expires_at' => Carbon::parse(
+                    $tokenResult->token->expires_at
+                )->toDateTimeString()
+            ];
         }
 
         return ['status' => 'failed',];
