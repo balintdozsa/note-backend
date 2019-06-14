5535354c0a43131aa1a5f4276631ff8dabc6c701
Bálint Dózsa <balint.do89@gmail.com>
2019-06-14 17:29:38 +0200

    Fixes

--- app/Utils/TimeRecognition.php
+++ app/Utils/TimeRecognition.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Utils;
-
 
 use Carbon\Carbon;
 
@@ -14,13 +12,15 @@ class TimeRecognition
         $patterns = [];
         $patterns[] = "/\d{4}\-\d{2}\-\d{2}\ \d{2}\:\d{2}/"; // 2010-01-01 12:15
         $patterns[] = "/\d{4}\.\d{2}\.\d{2}\ \d{2}\:\d{2}/";
-        $patterns[] = "(?<!\d{2}\ )\d{2}\:\d{2}"; // 14:40 without two number and space before it
+        $patterns[] = "/(?<!\d{2}\ )\d{2}\:\d{2}/"; // 14:40 without two number and space before it
 
         $matches = [];
         foreach ($patterns as $pattern) {
             $m = [];
             preg_match_all($pattern, $content, $m);
-            array_push($matches, ...$m[0]);
+            if (isset($m[0]) && count($m[0])) {
+                array_push($matches, ...$m[0]);
+            }
         }
 
         foreach ($matches as $time) {
